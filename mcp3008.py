61720802e5109e1fc4f342a97115799d720d3416
Yazid <yazidapple13@gmail.com>
2017-04-25 23:25:08 +0800

    Update mcp3008.py

--- mcp3008.py
+++ mcp3008.py
@@ -5,10 +5,12 @@ pot = MCP3008(0) # Pot is connected to CH0
 ldr = MCP3008(1) # LDR is connected to CH1
 led1 = LED(21)
 
+thresh = 0.5 # set threshold level to differentiate between light and dark
+
 while True:
 	print("Pot:{:.2f}  LDR:{:.2f}".format(pot.value,ldr.value))
 	sleep(0.5)
-	if ldr.value < 0.5:
+	if ldr.value < thresh:
 		led1.on()
 	else:
 		led1.off()
